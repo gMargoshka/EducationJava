020af286e33f1ebac9bda2a009d3000ad6d2dcd0
Gorbatsevich Margarita <g.margoshka13@yandex.ru>
2025-08-27 10:50:47 +0300

    update homework

--- src/work_08_08_2025/homework_26_08_2025/Main.java
+++ src/work_08_08_2025/homework_26_08_2025/Main.java
@@ -1,32 +1,58 @@
 package work_08_08_2025.homework_26_08_2025;
 
+import work_28_07_25.BankException;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        Instrument[] orchestra = new Instrument[5];
-        orchestra[0] = new Drums("Барабан","ударные");
-        orchestra[1] = new Guitar("Ибанез", "струнные",7);
-        orchestra[2] = new Violin("Виолончель","струнные",5);
-        orchestra[3] = new Flute("Флейта","духовые");
-        orchestra[4] = new Saxophone("Саксофон","духовые");
 
-        StringInstrument[] tunable = new StringInstrument[2];
-        tunable[0] = new Guitar("Ибанез", "струнные",7);
-        tunable[1] = new Violin("Виолончель","струнные",5);
+            Instrument[] orchestra = new Instrument[5];
+            orchestra[0] = new Drums("Барабан", "ударные");
+            orchestra[1] = new Guitar("Ибанез", "струнные", 7);
+            orchestra[2] = new Violin("Виолончель", "струнные", 5);
+            orchestra[3] = new Flute("Флейта", "духовые");
+            orchestra[4] = new Saxophone("Саксофон", "духовые");
 
-        System.out.println("Состав оркестра:");
+            StringInstrument[] tunable = new StringInstrument[2];
+            tunable[0] = new Guitar("Ибанез", "струнные", 7);
+            tunable[1] = new Violin("Виолончель", "струнные", 5);
 
-        for (Instrument instrument : orchestra) {
-            System.out.println("--------------------------------------");
-            System.out.println(instrument.toString());
+        try {
+
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Введите количество струн");
+            int strings = scanner.nextInt();
+
+            for (StringInstrument instrument : tunable) {
+                if (strings == instrument.numberOfStrings) {
+                    System.out.println("**************************************");
+                    System.out.println(instrument);
+                    System.out.println("**************************************");
+                }
+            }
+        } catch (InputMismatchException ime) {
+            System.out.println(ime.getMessage());
+              throw new TunableException("Некорректно введено количество струн!");
         }
 
-        System.out.println(" \n ");
 
-        System.out.println("Состав настраиваемых инструментов:");
+            System.out.println("Состав оркестра:");
 
-        for (StringInstrument instrument : tunable) {
-            System.out.println("++++++++++++++++++++++++++++++++++++++");
-            System.out.println(instrument);
-        }
+            for (Instrument instrument : orchestra) {
+                System.out.println("--------------------------------------");
+                System.out.println(instrument.toString());
+            }
+
+            System.out.println(" \n ");
+
+            System.out.println("Состав настраиваемых инструментов:");
+
+            for (StringInstrument instrument : tunable) {
+                System.out.println("++++++++++++++++++++++++++++++++++++++");
+                System.out.println(instrument);
+            }
+
     }
 }
---
+++ src/work_08_08_2025/homework_26_08_2025/TunableException.java
@@ -0,0 +1,7 @@
+package work_08_08_2025.homework_26_08_2025;
+
+public class TunableException extends RuntimeException {
+    public TunableException(String message) {
+        super(message);
+    }
+}
